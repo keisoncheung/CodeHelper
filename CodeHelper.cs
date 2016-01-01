f461bd2840c54353cab14162e621232df917d19d
keisoncheung <accpzyyx@126.com>
2016-01-01 18:08:52 +0800

    首次版本
    首次版本

---
+++ CodeHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace trainNewSun
+{
+    public static class CodeHelper
+    {
+        /// <summary>
+        /// 一键分享微博
+        /// </summary>
+        /// <returns></returns>
+        public static string ShareWeibo()
+        {
+            StringBuilder sbr = new StringBuilder();
+            sbr.Append(@"<a class='bshareDiv' href='http://www.bshare.cn/share'>分享按钮</a><script type='text/javascript' charset='utf-8' src='http://static.bshare.cn/b/buttonLite.js#uuid=&amp;style=3&amp;fs=4&amp;textcolor=#fff&amp;bgcolor=#F60&amp;text=分享到&amp;pophcol=3'></script>");
+            return sbr.ToString();
+        }
+
+ 
+    }
+}
---
+++ RexHelper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PublicHelper 
+{
+    public static class RexHelper
+    {
+        public enum rexType
+        {
+            /// <summary>
+            /// 手机号码 
+            /// </summary>
+            Phone,
+            /// <summary>
+            /// 电话号码
+            /// </summary>
+            Tel,
+            /// <summary>
+            /// 电子邮箱
+            /// </summary>
+            Email,
+             
+        }
+
+        private static string PatternPhone { get { return @"(^(01|1)[3,4,5,8][0-9])\d{8}$"; } }
+
+        /// <summary>
+        /// 正则匹配
+        /// </summary>
+        public static Func<string, rexType, bool> IsMatch = (str, rex) => { return Regex.IsMatch(str, GetPattern(rex)); };
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private static Func<rexType, string> GetPattern = g =>
+        {
+            string pattern = string.Empty;
+            switch (g)
+            {
+                case rexType.Phone:
+                    pattern = PatternPhone;
+                    break;
+                default:
+                    pattern = PatternPhone;
+                    break;
+            }
+            return pattern;
+        };
+    }
+}
